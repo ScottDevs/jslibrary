185e34ec67fc2ca61d1f5b4b904d5cddd40a1852
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 10:44:22 -0400

    1.20_booleans.js

---
+++ js/1.20_booleans.js
@@ -0,0 +1,11 @@
+//Booleans
+//True and False Statements
+var myAge = 21;
+var paulsAge = 39;
+
+//I am not older then Paul so False
+console.log(myAge > paulsAge);
+//I am older then Paul so True
+console.log(myAge < paulsAge);
+
+
