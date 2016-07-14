d3d9b3620f41e7f94a822e6801f25fe31b664fc1
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 10:28:32 -0400

    1.00_integers.js

---
+++ js/1.00_integers.js
@@ -0,0 +1,18 @@
+//When the Variable myAge is used the answer will be equal to 21.
+var myAge = 21;
+var myDeath = 101;
+var myFavNum = 7;
+var myBirthMonth = 7;
+var myBirthDay = 7;
+
+//Calls the variable.
+console.log(myAge);
+console.log(myDeath);
+console.log(myFavNum);
+console.log(myBirthMonth);
+console.log(myBirthDay);
+
+//Adds two variables together
+//Can do same with any math
+console.log(myBirthDay + myAge);
+console.log("My birthday is in July which is the ", + myBirthMonth + "th month of the year." )
