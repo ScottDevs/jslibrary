031b8a72b6a7d2aad7d9941dd1799fa891be5f48
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 17:11:15 -0400

    Objects Inheritance Update

--- js/1.72_objects_inheritance.js
+++ js/1.72_objects_inheritance.js
@@ -1,13 +1,54 @@
-// Object Inheritance
+//Rank Inheritance
+var iron = {
+	rank : "Rank: Iron",
+	homes: "Homes: One", 
+	food : "Food: Sixteen Steak",
+	rankBreak : "---",
 
-function diamondRank (perms, chat, level) {
-	this.permissions = perms;
-	this.chatColor = chat;
-	this.serverLevel = level;
+	printName : function(){
+		console.log(this.rank);//Test
+	}
+
 }
 
-var panda = new diamondRank ("Diamond", "Red", 46);
-var fred = new diamondRank ("Diamond", "Blue", 85);
+var iron = Object.create(iron);
+				
+var gold = Object.create(iron);
+		gold.rank = "Rank: Gold";
+		gold.homes = "Homes: Two";
+		gold.weapons = "Weapon: Diamond Sword";
+		
+		
 
-console.log(panda); //Displays Panda rank stats
-console.log(panda.permissions); //Displays Panda's Permissions
+var diamond = Object.create(gold);
+		diamond.rank = "Rank: Diamond";
+		diamond.homes = "Homes: Three";
+		diamond.command = "Commands: Abilty to Fly"
+		
+// Iron
+	console.log(iron.rank);				
+	console.log(iron.homes);			
+	console.log(iron.food);				
+	// Break
+		console.log(iron.rankBreak);
+// Gold
+	console.log(gold.rank);				
+	console.log(gold.homes);			
+	console.log(gold.food);				
+	console.log(gold.weapons);
+	// Break
+		console.log(gold.rankBreak);
+// Diamond
+	console.log(diamond.rank);
+	console.log(diamond.homes);
+	console.log(diamond.food);
+	console.log(diamond.weapons);
+	console.log(diamond.command);
+	// Break
+		console.log(diamond.rankBreak);
+
+
+
+
+
+
