3f709a312ee79b5b9443771d6ae2dfee9ad7f19a
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 13:24:23 -0400

    1.51_forloops.js

--- js/1.51_forloops.js
+++ js/1.51_forloops.js
@@ -1,6 +1,6 @@
 //For Loops
-//Write a for loop for an auction: Start the bid at 10, the increment is by 20, and the max bid is 500.
-for(var i=10;i<500;i+=20){
+//Write a for loop for an auction: Start the bid at 10, the increment is by 20, and the max bid is 400.
+for(var i=10;i<400;i+=20){
 	console.log(i)
 }
 
