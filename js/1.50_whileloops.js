b9568ada5c0d5c382e7b0f9409694136a58e62ea
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 13:23:03 -0400

    1.50_whileloops.js

--- js/1.50_whileloops.js
+++ js/1.50_whileloops.js
@@ -1,10 +1,11 @@
 //While Loop
-//Make a variable called: Votes
-//A player can vote up to 50.
+//Make a variable called: roomCapacity
+//A room can hold up to 50 people.
+var roomCapacity = 0; //Starting point is 0.
 
-
-
-
-
-
-var votes =
+while(roomCapacity < 50){
+	roomCapacity++;
+	console.log(roomCapacity + ":" + " People currently in the room.");
+	//This uses a boolean of true or false. 
+	//as long as roomCapacity is less than 50, then add++ (1) until it reaches 50.
+}
