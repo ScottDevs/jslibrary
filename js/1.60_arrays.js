759cddc68c0f5f33988ec5a6c8898ecf06332718
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 13:31:53 -0400

    1.60_array.js

---
+++ js/1.60_arrays.js
@@ -0,0 +1,10 @@
+//Arrays
+
+var cookies = [
+	"Chocolate Chip",  	//0
+	"M&M",				//1
+	"Sugar",			//2
+	"Peanut Butter"		//3
+]
+console.log(cookies);	//Calls all the cookies
+console.log(cookies[2]);//Calls the cookie 2nd value cookie.
