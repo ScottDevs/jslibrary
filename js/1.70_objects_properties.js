b2c5cdaf12229b8c3acf89d6952ec3567af0b54a
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 11:21:27 -0400

    1.70_objects_properties.js

---
+++ js/1.70_objects_properties.js
@@ -0,0 +1,12 @@
+//Object Properties
+
+var bike = {
+	//Properties
+	type: "Off Road",
+	wheelSize: 29,
+	color: "black",
+	size: "XL",
+
+}
+console.log(bike); //Shows all Properties
+console.log(bike.type); //Shows the variable bike then the property type of the bike.
