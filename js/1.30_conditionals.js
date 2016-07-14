f4c1123af6c4b42d406d8aeb8caa8914d3a2ad34
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-14 14:32:49 -0400

    1.30_conditionals.js

---
+++ js/1.30_conditionals.js
@@ -0,0 +1,11 @@
+var relationShipStatus = "single"; 
+//Variable has quotes due to it bein a string
+if (relationShipStatus == "single") { //If gender equals male say below.
+  console.log("You will find the right women for you.");
+} else {
+    //if not equal say this instead.
+  console.log("Glad you found someone!");
+}
+
+
+
